05054c24048e1ab4ea6f89f1ba32e3859eb2d1ee
unknown <b.yunakov1654@gmail.com>
2023-04-25 21:52:40 +0300

    auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  root to: "home#index"
   resources :user_records
   resources :users
   resources :genres
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_08_182751) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_20_090641) do
   create_table "authors", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.text "bio"
@@ -60,6 +60,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_08_182751) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "books", "authors"
