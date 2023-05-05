692c8a8c994b619b5f2eedb544564d3799e89fc4
16Bogdan54 <b.yunakov1654@gmail.com>
2023-05-05 10:38:42 +0300

    authors pagination&table added

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,7 +3,7 @@ class AuthorsController < ApplicationController
 
   # GET /authors or /authors.json
   def index
-    @authors = Author.all
+    @authors = Author.page(params[:page] || 1).per(5)
   end
 
   # GET /authors/1 or /authors/1.json
