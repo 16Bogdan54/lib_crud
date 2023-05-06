f8b66222f98af8b0bddbcc459d03445783b978b4
16Bogdan54 <b.yunakov1654@gmail.com>
2023-05-06 20:20:07 +0300

    sort  implemented

--- app/queries/users_query.rb
+++ app/queries/users_query.rb
@@ -9,7 +9,13 @@ class UsersQuery
   def results_name
     users = User.all
     users = users.where("concat(name, ' ', surname) LIKE ?", "%#{search}%") if search.present?
-    users.order(created_at:  :asc || :sort)
+    if sort.present?
+      direction = %w[asc desc].include?(sort) ? sort : 'asc'
+      users = users.order(Arel.sql("concat(name, ' ', surname) #{direction}"))
+    else
+      users = users.order(created_at: :asc)
+    end
+    users
   end
 
 end
