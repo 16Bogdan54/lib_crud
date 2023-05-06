b0d02df735263bf162dc35976bf35243419ff9f5
16Bogdan54 <b.yunakov1654@gmail.com>
2023-05-06 11:33:05 +0300

    model

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,13 +4,13 @@ class Book < ApplicationRecord
   has_many :genres
 
   def self.add
-    (1..5).each { |i|
+    (1..1).each { |i|
 
       input = Book.new(title: "title #{i+2}",
                        description: "desc #{i+2}",
                        library_id: Library.find(2).id,
-                       author_id: Author.find(3).id,
-                       genre_id: Genre.find(2).id,
+                       author_id: Author.find(2).id,
+                       genre_id: Genre.find(4).id,
                        created_at: Time.now,
                        updated_at: Time.now)
       input.save
