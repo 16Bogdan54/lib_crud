95c8cb975e2ff70b0962ba77299129bdcbffcfd6
16Bogdan54 <b.yunakov1654@gmail.com>
2023-05-06 21:41:30 +0300

    search&sort for age implemented

--- app/queries/users_query.rb
+++ app/queries/users_query.rb
@@ -1,14 +1,16 @@
 class UsersQuery
-  attr_accessor :search, :sort
+  attr_accessor :full_name, :age, :sort
 
-  def initialize(search = nil, sort = nil)
-    @search = search
+  def initialize(full_name = nil, age = nil, sort = nil)
+    @full_name = full_name
+    @age = age
     @sort = sort
   end
 
   def results_name
     users = User.all
-    users = users.where("concat(name, ' ', surname) LIKE ?", "%#{search}%") if search.present?
+    users = users.where("concat(name, ' ', surname) LIKE ?", "%#{full_name}%") if full_name.present?
+    users = users.where(age: age) if age.present?
     if sort.present?
       direction = %w[asc desc].include?(sort) ? sort : 'asc'
       users = users.order(Arel.sql("concat(name, ' ', surname) #{direction}"))
